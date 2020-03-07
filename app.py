9aa29851f335880d545233f0316853c1ddc1a403
Roman M. Gerasimov <romagrizly@gmail.com>
2020-03-08 03:05:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,23 @@ def main():
     text_reader = TextReader('Данные для задания.csv', 'csv')
     text_filter = TextFilter()
 
+    # считывание данных из 'Данные для задания.csv'
     text = ' '.join(map(lambda x: x['body'], text_reader.read()))
+
+    # результат работы парсинга
     iter_obj = text_filter.parse_text(text)
+
+    # построение алфавитно-частнтного словаря
     dict_res = {}
     for elem in iter_obj:
         if elem not in dict_res:
             dict_res[elem] = 0
         dict_res[elem] += 1
 
+    # вывод алфавитно-частотного словаря, отсортированного по количеству употребления слов, в 'answer.txt'
     with open("answer.txt", 'w') as output_file:
         for key, value in sorted(dict_res.items(), reverse=True, key=lambda x: x[1]):
-            print(f"{key}: {value}", file=output_file)
+            print(f"{value}: {key}", file=output_file)
 
 
 if __name__ == '__main__':
